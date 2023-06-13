3da095de479bd74ade21fdc0d775ef52ca24fe99
joanalawer <lawerjoana477@gmail.com>
2023-06-13 12:36:51 +0000

    Queues

--- index.js
+++ index.js
@@ -144,3 +144,17 @@ function timesFive(num){
     return num * 5;
 }
 console.log(timesFive(10));
+
+
+// QUEUES 
+/* Remove first item of arrary and return item; 
+Add new item to end of array ansd print new array */
+function nextInLine(arr, item){
+    arr.push(item);
+    return arr.shift();
+}
+var testArr = [1,2,3,4,5];
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
--- test.js
+++ test.js
@@ -1,5 +1,12 @@
-// Using return
-function timesFive(num){
-    return num * 5;
+// QUEUES 
+/* Remove first item of arrary and return item; 
+Add new item to end of array ansd print new array */
+function nextInLine(arr, item){
+    arr.push(item);
+    return arr.shift();
 }
-console.log(timesFive(10));
+var testArr = [1,2,3,4,5];
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));

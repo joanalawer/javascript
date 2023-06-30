42e140aab0f4de065cbaa45b19aad1331147a13f
joanalawer <lawerjoana477@gmail.com>
2023-06-30 16:55:21 +0000

    Remove quotation marks from numbers

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
 function multipleCaseInput(val) {
     var answer = "";
     switch(val) {
-        case "1":
-        case "2":
-        case "3":
+        case 1:
+        case 2:
+        case 3:
             answer = "Good";
             break;
-        case "4":
-        case "5":
-        case "6":
+        case 4:
+        case 5:
+        case 6:
             answer = "Better";
             break;
-        case "7":
-        case "8":
-        case "9":
+        case 7:
+        case 8:
+        case 9:
             answer = "Best";
             break;
         // default:

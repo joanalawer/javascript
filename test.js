62cd471316f8ea4fe808682009e55c1fa28b1019
joanalawer <lawerjoana477@gmail.com>
2023-06-30 21:18:41 +0000

    Objects in javascript

--- test.js
+++ test.js
@@ -1,31 +1,10 @@
-/*  COUNTING CARDS */
-var count = 0;
-
-function cc(card) {
-    switch(card) {
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-            count++;
-            break;
-        case 10:
-        case "J":
-        case "Q":
-        case "K":
-        case "A":
-            count--;
-            break;
-    }
-
-    var holdbet = 'Hold'
-    if (count > 0) {
-        holdbet = 'Bet'
-    }
-
-    return count + " " + holdbet;
-}
-
-cc(2); cc('K'); cc(10); cc('K'); cc('A');
-console.log(cc(4));
+/*  BUILD OBJECTS IN JAVASCRIPT */
+var myList = {
+    "Food": "Waakye",
+    "Books": "Mystery",
+    "Savings": $3000000,
+    "Vacation": "Greece"
+};
+// myList is the object
+// Food, Books, Savings, Vacation are called property
+// Waakye, Mystery, $3000000, Greece are values

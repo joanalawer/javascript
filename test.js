2010f71769eec8ebf42ca225db8014f6a2adcd5f
joanalawer <lawerjoana477@gmail.com>
2023-06-30 17:09:10 +0000

    Function return boolean values

--- test.js
+++ test.js
@@ -1,27 +1,5 @@
-function multipleCaseInput(val) {
-    var answer = "";
-    switch(val) {
-        case 1:
-        case 2:
-        case 3:
-            answer = "Good";
-            break;
-        case 4:
-        case 5:
-        case 6:
-            answer = "Better";
-            break;
-        case 7:
-        case 8:
-        case 9:
-            answer = "Best";
-            break;
-        default:
-            answer = "Grade";
-            break;
-    }
-
-    return answer;
+function multipleCaseInput(a, b) {
+   return a < b;
 }
 
-console.log(multipleCaseInput(0));
+console.log(multipleCaseInput(5, 10));

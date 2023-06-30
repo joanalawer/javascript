763f9541ebc8ff51f2d77d9e46731c03f8e11793
joanalawer <lawerjoana477@gmail.com>
2023-06-30 18:02:20 +0000

    Counting cards

--- index.js
+++ index.js
@@ -271,3 +271,34 @@ function caseInSwitch(val) {
 }
 /* ************************************************************************* */
 
+/* ************************************************************************* */
+/*  COUNTING CARDS */
+var count = 0;
+
+function cc(card) {
+    switch(card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++;
+            break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--;
+            break;
+    }
+
+    var holdbet = 'Hold'
+    if (count > 0) {
+        holdbet = 'Bet'
+    }
+
+    return coint + " " + holdbet;
+}
+
+cc(2);

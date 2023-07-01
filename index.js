ffdb91510e640f9ee0377f402f7bc80abb2605d6
joanalawer <lawerjoana477@gmail.com>
2023-07-01 14:27:33 +0000

    Nested objects

--- index.js
+++ index.js
@@ -414,3 +414,23 @@ var myMusicCollection = [
         "gold": false
     }
 ]
+/* ************************************************************************* */
+
+
+/* ************************************************************************* */
+/* Acessing Nested Objects */
+ var myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
+    }
+ };
+
+ var gloveBoxContent = myStorage.car.inside["glove box"];
+
+ console.log(gloveBoxContent)

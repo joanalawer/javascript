578ce07a10f0845de8995a49bc22b28ee9706a1c
joanalawer <lawerjoana477@gmail.com>
2023-07-01 15:16:42 +0000

    Acessing nested objects

--- index.js
+++ index.js
@@ -434,3 +434,26 @@ var myMusicCollection = [
  var gloveBoxContent = myStorage.car.inside["glove box"];
 
  console.log(gloveBoxContent)
+
+ /* Accessing Nested Objects */
+ var myPlants = [
+    {
+        type: "flowers",
+        list:[
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+ ];
+ 
+ var secondTree = myPlants[1].list[1]
+console.log(secondTree);

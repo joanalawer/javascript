9afb34da0e4a69a7312307d30ad3166df4154d6b
joanalawer <lawerjoana477@gmail.com>
2023-07-02 21:26:32 +0000

    Iterate with for loop

--- index.js
+++ index.js
@@ -467,7 +467,7 @@ var myArray = [];   // declare an empty array list
 var i = 0;          // declare and initialise a counter i to 0
 while (i < 10) {    // set condition to 10
     myArray.push(i);// append list with initial counter 
-    i++;            // increment count while alue is less than 10
+    i++;            // increment count while value is less than 10
 }
 
 console.log(myArray); // display final list
--- test.js
+++ test.js
@@ -1,10 +1,9 @@
-/*ITERATE WITH WHILE LOOP*/
+/*ITERATE WITH for LOOP*/
 var myArray = [];   // declare an empty array list
 
-var i = 0;          // declare and initialise a counter i to 0
-while (i < 10) {    // set condition to 10
-    myArray.push(i);// append list with initial counter 
-    i++;            // increment count while value is less than 10
+// var i = 0;          // declare and initialise a counter i to 0
+for (var i = 0, i < 10, i++;) {    // set condition to 10
+    myArray.push(i);// append list with initial counter increment count while value is less than 10
 }
 
 console.log(myArray); // display final list

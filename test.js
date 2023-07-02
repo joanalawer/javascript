5693a0fdfcc1d0d406467658beb533f71740d427
joanalawer <lawerjoana477@gmail.com>
2023-07-02 21:27:50 +0000

    Put semicolon in loop condition

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 var myArray = [];   // declare an empty array list
 
 // var i = 0;          // declare and initialise a counter i to 0
-for (var i = 0, i < 10, i++;) {    // set condition to 10
+for (var i = 0; i < 10; i++) {    // set condition to 10
     myArray.push(i);// append list with initial counter increment count while value is less than 10
 }
 

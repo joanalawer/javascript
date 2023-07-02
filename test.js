3105fc260b60f08ccd04da5ad8cace9f5df9165e
joanalawer <lawerjoana477@gmail.com>
2023-07-02 21:31:52 +0000

    Iterate with for loop decrement

--- test.js
+++ test.js
@@ -2,8 +2,8 @@
 var myArray = [];   // declare an empty array list
 
 // var i = 0;          // declare and initialise a counter i to 0
-for (var i = 0; i < 10; i++) {    // set condition to 10
-    myArray.push(i);// append list with initial counter increment count while value is less than 10
+for (var i = 10; i > 0; i--) {    // set condition to 10
+    myArray.push(i);// append list with initial counter decrement count while value is less than 10
 }
 
 console.log(myArray); // display final list

653c51923e6e1e9c0a7098fc86447cad33da2226
joanalawer <lawerjoana477@gmail.com>
2023-07-02 21:39:37 +0000

    Counting backwards with for loop

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-/*ITERATE odd numbers WITH for LOOP*/
+/*ITERATE backwards even numbers WITH for LOOP*/
 var myArray = []; 
 
 // var i = 0;      
-for (var i = 0; i < 10; i += 2) {    
+for (var i = 10; i > 0; i -= 2) {    
     myArray.push(i);
 }
 

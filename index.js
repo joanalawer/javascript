27e44d6c82e73c04772b5a269c549a1fe6d3d03f
joanalawer <lawerjoana477@gmail.com>
2023-09-01 22:09:51 +0000

    var and let scopes

--- index.js
+++ index.js
@@ -570,3 +570,16 @@ function catTalk(){
 
 }
 catTalk();
+/* Compare Scopes of the var and let keywords */
+function checkScope() {
+    "use strict";
+    var i = "function scope";
+    if (true) {
+        i = "block scope";
+        console.log("Block scope i is: ", i);
+    }
+    console.log("Function scope i is: ", i);
+    return i;
+}
+
+checkScope();

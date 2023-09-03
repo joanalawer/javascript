9c5cca3b364ad1aa2f0cb9a136b5e6010c47ddf7
joanalawer <lawerjoana477@gmail.com>
2023-09-03 09:34:13 +0000

    Test with finite numbers

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const realNumberArray = [4, 5.5, -9.8, 3.14, 42, 6, 8.34, -2];
 
 const squareList = (arr) => {
-    const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x );
+    const squaredIntegers = arr.filter(num => Number.isFinite(num) && num > 0).map(x => x * x );
     return squaredIntegers;
 };
 

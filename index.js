09ced57da9e490545b4295bd40c38e6b88b27b14
joanalawer <lawerjoana477@gmail.com>
2023-09-03 14:08:13 +0000

    Destructuring nested objects

--- index.js
+++ index.js
@@ -711,3 +711,19 @@ function getTempOfTmrw(avgTemperatures) {
 }
 
 console.log(getTempOfTmrw(AVG_TEMPERATURE));
+
+// Destructuring assignment with nested objects
+const LOCAL_FORECAST = {
+    today: { min:72, max:83},
+    tomorrow: {min:73.3, max:84.6}
+};
+
+function getMaxOfTmrw(forecast) {
+    "use strict";
+
+    const { tomorrow: {max: maxOfTomorrow }} = forecast;    // chnge occurs here
+
+    return maxOfTomorrow;
+}
+console.log(getMaxOfTmrw(LOCAL_FORECAST));
+

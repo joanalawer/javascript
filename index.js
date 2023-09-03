e1a1abeea163b3e4555ddc47df2d60cf82420f79
joanalawer <lawerjoana477@gmail.com>
2023-09-03 21:46:53 +0000

    Class to define constructor function

--- index.js
+++ index.js
@@ -772,3 +772,16 @@ const bicycle = {
 
 bicycle.setGear(3);
 console.log(bicycle.gear);
+
+//  Use class syntax to define a constructor function
+function makeClass() {
+    class Vegetable {
+        constructor(name){
+            this.name = name;
+        }
+    }
+    return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name);
--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-// Create strings using templates literals
-const person = {
-    name: "Zodiac Hasbro",
-    age: 56
-};
-
-// Template literal with multiline and string interpolation
-const greeting = `Hello, name is ${person.name}!
-I am ${person.age} years old.`;
-
-console.log(greeting);
+//  Use class syntax to define a constructor function
+function makeClass() {
+    class Vegetable {
+        constructor(name){
+            this.name = name;
+        }
+    }
+    return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name);

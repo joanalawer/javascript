1b1e646ea5fe16913f6938f0ed9c5f0983008255
joanalawer <lawerjoana477@gmail.com>
2023-09-03 09:30:28 +0000

    Arrow functions and higher order functions

--- index.js
+++ index.js
@@ -639,4 +639,14 @@ const myConcat = (arr1, arr2) => arr1.concat(arr2);
 
 console.log(myConcat([1, 2], [3, 4, 5]));
 /* Write higher order arrow functions */
-// .filter, .map, 
+// .filter, .map, .reduce
+// whenever one function takes another function as argument, arrow function is used
+const realNumberArray = [4, 5.5, -9.8, 3.14, 42, 6, 8.34, -2];
+
+const squareList = (arr) => {
+    const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x );
+    return squaredIntegers;
+};
+
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);

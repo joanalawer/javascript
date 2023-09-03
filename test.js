e78eb5112359ec592f8c49e0f4cc509f45315da5
joanalawer <lawerjoana477@gmail.com>
2023-09-03 12:19:40 +0000

    Test rest operator

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
-const realNumberArray = [4, 5.5, -9.8, 3.14, 42, 6, 8.34, -2];
-
-const squareList = (arr) => {
-    const squaredIntegers = arr.filter(num => Number.isFinite(num) && num > 0).map(x => x * x );
-    return squaredIntegers;
-};
-
-const squaredIntegers = squareList(realNumberArray);
-console.log(squaredIntegers);
+const sum = (function() {
+    // return function sum(x, y, z) { 
+    //     const args = [x, y, z];
+    return function sum(...args) {
+        return args.reduce((a, b) => a + b, 0);
+    };
+})(); 
+console.log(sum(1, 2, 3, 7));

77b4d7320a792533b3c9ba9931668843a8d94c43
joanalawer <lawerjoana477@gmail.com>
2023-09-03 22:31:36 +0000

    Export , import

--- test.js
+++ test.js
@@ -1,22 +1,5 @@
-// Use getters and setters to control access to an object
-function makeClass() {
-    class Thermostat {
-       constructor(temp) {
-           this._temp = 5/9 * (temp - 32);
-       }
-       get temperature() {
-           return this._temp;
-       }
-       set temperature(updatedTemp) {
-           return this._temp = updatedTemp;
-       }
-    }
-    return Thermostat;
-   }
+import { capitalizeString } from "./string_function";
 
-const Thermostat = makeClass();
-const thermos = new Thermostat(76);
-let temp = thermos.temperature;
-thermos.temperature = 26;
-temp = thermos.temperature;
-console.log(temp);
+const cap = capitalizeString("hello");
+
+console.log(cap);
